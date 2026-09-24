43db1f535e492dc03ca768fbd7cd46395dcb00fb
Ileana Sanabria <74793571+ilean01@users.noreply.github.com>
2026-09-24 12:38:04 -0300

    Cachear integración de Supabase en la PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "info1-shell-v1";
-const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE_NAME = "info1-shell-v2";
+const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./supabase-config.js", "./cloud-sync.js", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
