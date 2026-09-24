c4b7425be672f3afb2b8254a7eabfaa40d1f8fff
Ileana Sanabria <74793571+ilean01@users.noreply.github.com>
2026-09-24 19:27:22 -0300

    Bump PWA cache after cloud sync fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'info1-pwa-v3';
+const CACHE = 'info1-pwa-v4';
 const CORE = ['./','./index.html','./manifest.webmanifest','./supabase-config.js','./cloud-sync.js','./supabase-media-bridge.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
