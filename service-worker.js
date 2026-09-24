e0b454cdb1cf4ddce0b169bb5056c895db93c063
Ileana Sanabria <74793571+ilean01@users.noreply.github.com>
2026-09-24 18:22:46 -0300

    Cache Supabase media bridge in INFO1 PWA

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'info1-pwa-v2';
-const CORE = ['./','./index.html','./manifest.webmanifest','./supabase-config.js','./cloud-sync.js','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE = 'info1-pwa-v3';
+const CORE = ['./','./index.html','./manifest.webmanifest','./supabase-config.js','./cloud-sync.js','./supabase-media-bridge.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', event => {
